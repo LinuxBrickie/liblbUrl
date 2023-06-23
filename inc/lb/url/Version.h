5de730e10e05803bae440abc6c4e3d4c271c7cde
Paul Fotheringham <fotheringham.paul@gmail.com>
2023-06-23 16:30:52 +0100

    Add a versioning information to the library.
    Upcoming commit will add WebSocket support which requires libcurl 7.86.0
    or later so it would be nice to say what version we are are at and what
    it supports.

---
+++ inc/lb/url/Version.h
@@ -0,0 +1,51 @@
+/*
+    Copyright (C) 2023  Paul Fotheringham (LinuxBrickie)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIB_LB_URL_VERSION_H
+#define LIB_LB_URL_VERSION_H
+
+
+// Version macros
+#define LIB_LB_URL_VERSION "1.0.0"
+#define LIB_LB_URL_VERSION_MAJOR 1
+#define LIB_LB_URL_VERSION_MINOR 0
+#define LIB_LB_URL_VERSION_PATCH 0
+#define LIB_LB_URL_VERSION_NUM 0x010000
+
+
+namespace lb
+{
+
+
+namespace url
+{
+
+/** \brief Contains library version information. */
+const struct Version
+{
+  const unsigned int number{ LIB_LB_URL_VERSION_NUM };
+}
+version;
+
+
+} // End of namespace url
+
+
+} // End of namespace lb
+
+
+#endif // VLIB_LB_URL_ERSION_H
